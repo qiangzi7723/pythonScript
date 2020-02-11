33ad4c4200742d3c84975181fc6a74981187ba91
Xiao LingDan <amiao@via.cool>
2020-02-12 02:15:55 +0800

    提交脚本 排列文件顺序

---
+++ sortFile.py
@@ -0,0 +1,32 @@
+import os
+
+# 文件目的：指定文件夹下，将文件按照文件容量降序排序，方便删除。
+
+def dictionairy(key_value):
+    new_kv = sorted(key_value.items(), key = lambda kv:(kv[1], kv[0]))
+    for key in new_kv:
+        print("文件名称：%s 文件大小：%.2f MB"%(key[0],key[1]))
+
+def get_FileSize(filePath):
+    fsize = os.path.getsize(filePath)
+    fsize = fsize/float(1024 * 1024)
+    return round(fsize, 2)
+
+def sort_file(file_dir):
+    name_list = []
+    size_list = []
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+        	name = os.path.join(root,file)
+        	size = get_FileSize(name)
+        	file_name = name.replace(file_dir,"")
+        	name_list.append(file_name)
+        	size_list.append(size)
+        	
+    obj = dict(zip(name_list,size_list))
+    dictionairy(obj)
+
+# 输入目录地址
+root = os.getcwd()
+sort_file(root)
+
